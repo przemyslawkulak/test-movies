ca5373322bfae2cf4d5480928679b79b2bebc17b
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-05-13 17:09:50 +0200

    routing

--- movies/src/app/details/details.component.ts
+++ movies/src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log('id', id);
   }
 
 }
